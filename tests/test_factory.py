6c032ea1cef24a8578443305ee7e381adb891966
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-01-18 23:59:13 +0700

    remove test config

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,27 +1,6 @@
 import os
 from web import create_app
 
-
-def test_config():
-    assert not create_app({
-        'PONY': {
-            'provider': 'postgres',
-            'user': os.environ.get('PG_USER', 'postgres'),
-            'password': os.environ.get('PG_PASSWORD', 'devpassword'),
-            'host': os.environ.get('PG_HOST', 'localhost'),
-            'database': os.environ.get('PG_DB_NAME', 'bookdb')
-        }
-    }).testing
-    assert create_app({'TESTING': True,
-                       'PONY': {
-                           'provider': 'postgres',
-                           'user': os.environ.get('PG_USER', 'postgres'),
-                           'password': os.environ.get('PG_PASSWORD', 'devpassword'),
-                           'host': os.environ.get('PG_HOST', 'localhost'),
-                           'database': os.environ.get('PG_DB_NAME', 'bookdb')
-                       }}).testing
-
-
 def test_hello(client):
     response = client.get('/hello/')
     assert b'Hello, World!' in response.data
